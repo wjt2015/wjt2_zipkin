ed4aeb03c9c96f80bda1c67f809968227e82242b
王锦涛 <jintao9@staff.weibo.com>
2021-02-08 11:54:31 +0800

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+if (@# > =1);then
+git add -A;git commit -m '$1';git push 
+else
 git add -A;git commit -m 'update';git push 
+fi
+
+
 mvn clean package -U -DskipTests=true 
 cd zipkin-server/target && java -jar $(find ./ -name "*-exec.jar") 
 
